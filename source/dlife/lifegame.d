60a873d9856c26249fd0de0070d6345df34cd3c2
outlandkarasu <outland.karasu@gmail.com>
2014-02-15 20:31:00 +0900

    ライフの描画をバッファリングした。

--- source/dlife/lifegame.d
+++ source/dlife/lifegame.d
@@ -7,11 +7,15 @@ import std.random : dice;
 
 import derelict.sdl2.sdl;
 
-import dlife.game : Game;
 import dlife.world : World;
+import dlife.game : Game;
+import dlife.buffer : Buffer;
 
 /// 初期配置時のライフ生成確率
 enum INITIAL_LIFE_PROPORTION = 0.5;
+
+/// 描画時の点バッファサイズ
+enum POINT_BUFFER_SIZE = 1000;
 
 /**
  *  ライフゲームの実装
@@ -29,7 +33,14 @@ class LifeGame : Game {
      */
     this(string title, uint width, uint height, uint fps ) {
         super(title, width, height, fps);
+
+        // 点バッファの生成
+        pointBuffer_ = Buffer!SDL_Point(POINT_BUFFER_SIZE, &drawPoints);
+
+        // ライフゲーム世界の生成
         world_ = new World(width, height);
+
+        // ランダムにライフを配置する
         for(uint y = 0; y < height; ++y) {
             for(uint x = 0; x < width; ++x) {
                 if(dice(INITIAL_LIFE_PROPORTION, 1.0 - INITIAL_LIFE_PROPORTION)) {
@@ -39,7 +50,6 @@ class LifeGame : Game {
         }
     }
 
-
     /**
      *  描画関数
      *
@@ -47,10 +57,14 @@ class LifeGame : Game {
      *      renderer = 描画用レンダラ
      */
     protected override void draw(SDL_Renderer* renderer) {
+        renderer_ = renderer;
         SDL_SetRenderDrawColor(renderer, Uint8.max, Uint8.max, Uint8.max, Uint8.max);
         foreach(x, y; world_) {
-            SDL_RenderDrawPoint(renderer, cast(int) x, cast(int) y);
+            pointBuffer_.add(SDL_Point(cast(int) x, cast(int) y));
         }
+
+        // 点バッファを描画
+        pointBuffer_.flush();
 
         // 次の時代へ
         world_.next();
@@ -58,7 +72,24 @@ class LifeGame : Game {
 
 private:
 
+    /**
+     *  点バッファの描画
+     *
+     *  Params:
+     *      points = 描画する点バッファ
+     */
+    void drawPoints(const(SDL_Point)[] points) {
+        SDL_RenderDrawPoints(
+                renderer_, points.ptr, cast(int) points.length);
+    }
+
+    /// 描画用レンダラ
+    SDL_Renderer* renderer_;
+
     /// ライフゲーム世界
     World world_;
+
+    /// 座標バッファ
+    Buffer!SDL_Point pointBuffer_;
 }
 
